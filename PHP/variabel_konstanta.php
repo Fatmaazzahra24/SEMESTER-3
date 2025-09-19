a7fe54f8ec2bb53c7c199f8cb38e74bdda7f095a
Fatmaazzahra24 <azzahrafatma727@gmail.com>
2025-09-19 14:28:02 +0700

    jobsheet4_PHP

---
+++ PHP/variabel_konstanta.php
@@ -0,0 +1,13 @@
+<?php 
+    $angka1 = 10;
+    $angka2 = 5 ;
+
+    $benar = true;
+    $salah = false;
+    echo "Variabel benar: $benar, <br> Variabel salah: $salah";
+    define("NAMA_SITUS", "WebsiteKu.com");
+    define("TAHUN_PENDIRIAN", 2023);
+
+    echo "<br> Selamat datang di " . NAMA_SITUS . ", situs yang idirikan pada tahun " .
+    TAHUN_PENDIRIAN . " . " ;
+?>
